bb60f5362b88af0c77a5d4d9a166b621bd7966bc
Abro1986 <AndrewBroestl@gmail.com>
2020-10-15 13:05:58 -0600

    moved routes folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const init = require('../routes/index');
+const init = require('./routes/index');
 
 const app = express();
 
