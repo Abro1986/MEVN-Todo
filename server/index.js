e89d2ec8d65839a802c6a9a960d762c6f525022a
Abro1986 <AndrewBroestl@gmail.com>
2020-10-16 22:27:51 -0600

    added condition to serve static assets

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const init = require('./routes/index');
+require('dotenv').config();
 
 const app = express();
 
@@ -10,6 +11,15 @@ app.use(bodyParser.json())
 app.use(cors());
 
 init(app);
+
+// Handle Production
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/public/'));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+}
 
 const port = process.env.PORT || 3000;
 
